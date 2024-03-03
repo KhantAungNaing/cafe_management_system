2dcc7778b300edfca0ea6a6dcfe45cc9f7445ca6
KhantAungNaing <khantaungnaing2001@gmail.com>
2024-03-03 12:02:40 +0630

    Product apis added

---
+++ src/main/java/com/inn/cafe/service/ProductService.java
@@ -0,0 +1,18 @@
+package com.inn.cafe.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+
+import com.inn.cafe.wrapper.ProductWrapper;
+
+public interface ProductService {
+
+	ResponseEntity<String> addNewProduct(Map<String, String> requestMap);
+
+	ResponseEntity<List<ProductWrapper>> getAllProducts();
+
+	ResponseEntity<String> updateProduct(Map<String, String> requestMap);
+
+}
--- src/main/java/com/inn/cafe/serviceImpl/CategoryServiceImpl.java
+++ src/main/java/com/inn/cafe/serviceImpl/CategoryServiceImpl.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import org.checkerframework.checker.units.qual.t;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
